028f95920183f924df3814504940f539b8de2237
elnemr <pro.ahmedelnemr@gmail.com>
2019-04-11 16:29:47 -0800

    ignore workspace

--- resources/views/backend/invoices/index.blade.php
+++ resources/views/backend/invoices/index.blade.php
@@ -7,15 +7,9 @@
     <div class="row text-center">
         <div class="col-md-6">
 {{--            <button id="add-new-product" class="btn btn-success" data-toggle="modal" data-target="#add-new">جديد</button>--}}
-           من <input type="date" name="from" class="form-group datepicker">
-            الي<input type="date" name="to" class="form-group datepicker">
+           {{--من <input type="date" name="from" class="form-group datepicker">--}}
+            {{--الي<input type="date" name="to" class="form-group datepicker">--}}
 
-            <div class='input-group date datepicker ' id='datetimepicker1'>
-                <input type='text' class="form-control" />
-                <span class="input-group-addon">
-                        <span class="glyphicon glyphicon-calendar"></span>
-                    </span>
-            </div>
         </div>
     </div>
     <br>
